e0fe508370b883fbbfa893088d149982b8d6dd67
keyminy <imanaifs@gmail.com>
2023-12-31 01:00:08 +0900

    S1.DI

---
+++ Services/DateTimeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UiDesktopApp1.Interfaces;
+
+namespace UiDesktopApp1.Services
+{
+	internal class DateTimeService : IDateTime
+	{
+		public DateTime? GetCurrentTime()
+		{
+			//현재 시간 return
+			return DateTime.Now;
+		}
+	}
+}
---
+++ ViewModels/Pages/DashboardViewModel.cs
@@ -0,0 +1,37 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using UiDesktopApp1.Interfaces;
+
+namespace UiDesktopApp1.ViewModels.Pages
+{
+	public partial class DashboardViewModel : ObservableObject
+	{
+		private readonly IDateTime _iDateTime;
+
+		[ObservableProperty]
+		private int _counter = 0;
+
+		[ObservableProperty]
+		private string currentTime = string.Empty;
+
+        public DashboardViewModel(IDateTime dateTime)
+        {
+			this._iDateTime = dateTime;
+		}
+
+        [RelayCommand]
+		private void OnCounterIncrement()
+		{
+			Counter++;
+		}
+
+		[RelayCommand]
+		private void OnTextChanged()
+		{
+			this.CurrentTime = this._iDateTime.GetCurrentTime().ToString();
+		}
+	}
+}
